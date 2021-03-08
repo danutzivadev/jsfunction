18afa95d19365cf755799823e37e462509c44104
dnz <danut.ivanescu@outlook.com>
2021-03-08 12:49:38 +0200

    Am adaugat mai multi parametrii la functia Random Number

--- random.js
+++ random.js
@@ -1,5 +1,14 @@
-function getRandomNumber() {
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
+function getRandomNumber(upper) {
+    var randomNumber = Math.floor(Math.random() * upper) + 1;
     return randomNumber;
 }
-alert(getRandomNumber());
+/*
+console.log(getRandomNumber(6));
+console.log(getRandomNumber(100));
+console.log(getRandomNumber(1000));
+*/
+function getArea(width, lenght, unit) {
+    var area = width * lenght;
+    return area + unit;
+}
+console.log(getArea(10,20, ' sq ft'));
