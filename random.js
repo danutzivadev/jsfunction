2e608317f5dad0361acd87a69449705a1633528c
dnz <danut.ivanescu@outlook.com>
2021-03-08 13:24:13 +0200

    random game, unde am oferit positbilitatea de a alege cel mai mic numar si cel mai mare

--- random.js
+++ random.js
@@ -1,14 +1,14 @@
-function getRandomNumber(upper) {
-    var randomNumber = Math.floor(Math.random() * upper) + 1;
+function getRandomNumber(upper, lower) {
+    var randomNumber = Math.floor(Math.random() * (upper - lower)) + lower;
     return randomNumber;
 }
-/*
-console.log(getRandomNumber(6));
-console.log(getRandomNumber(100));
-console.log(getRandomNumber(1000));
-*/
-function getArea(width, lenght, unit) {
+
+console.log(getRandomNumber(10,8));
+// console.log(getRandomNumber(100));
+// console.log(getRandomNumber(1000));
+
+/*function getArea(width, lenght, unit) {
     var area = width * lenght;
     return area + unit;
 }
-console.log(getArea(10,20, ' sq ft'));
+console.log(getArea(10,20, ' sq ft')); */
